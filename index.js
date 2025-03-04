8bfaf2f2d9b0322fb18280fa586bf3ea72fce2e5
MonicaRosales17 <monica.rosaleshernandez25@compscihigh.org>
2025-03-04 16:12:08 +0000

    lol

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express()
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path)
     next()
-  })
+})
   
-  app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"))
   
-  app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/index.html")
-  })
+})
   
+app.listen(3000, () => {
+    console.log("Server running")
+})
